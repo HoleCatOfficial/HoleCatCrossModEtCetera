2692aa50d4fa8bdfd562bc3ec9cd6b21bdc04714
HoleCat <ConstantineTheWyvern@gmail.com>
2025-09-08 19:58:20 -0400

    Updating Build

--- FranciumMultiCrossMod.cs
+++ FranciumMultiCrossMod.cs
@@ -5,12 +5,35 @@ using System.Text;
 using System.Threading.Tasks;
 using FranciumMultiCrossMod.Common;
 using Terraria.ModLoader;
+using FargowiltasSouls.Core;
+using FargowiltasSouls.Content.Items.Accessories.Souls;
+using FargowiltasSouls.Content.UI.Elements;
+using FargowiltasSouls.Core.Systems;
 
 namespace FranciumMultiCrossMod
 {
 	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class FranciumMultiCrossMod : Mod
 	{
-		
+	public static bool EternityModeEnabled = WorldSavingSystem.ShouldBeEternityMode;
+
+    public override object Call(params object[] args)
+    {
+        if (args.Length < 1)
+            return null;
+
+        string message = args[0] as string;
+
+        switch (message)
+        {
+            case "CheckEternity":
+                return EternityModeEnabled;
+
+            default:
+                Logger.Warn("Unknown call: " + message);
+                return null;
+        }
+    }
+
 	}
 }
